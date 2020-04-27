b1a0510486039f8f023f32fe08aba2390eb06557
matthew-devonport <roarsintheeast@gmail.com>
2020-04-27 22:39:20 +1200

    Set up routing of components on index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './index.css';
+
 import App from './components/App';
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import Checkout from './components/Checkout'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route component={App} path ="/" />
+            <Route component={SignIn} path ="/signin" />
+            <Route component={SignUp} path ="/signup" />
+            <Route component={Checkout} path ="/checkout" />
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
