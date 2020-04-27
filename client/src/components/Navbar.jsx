f2b30254a080a132ac8209926e31f53c200ec475
matthew-devonport <roarsintheeast@gmail.com>
2020-04-27 22:58:45 +1200

    added box and styles to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Box, Text } from 'gestalt'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
-    <div>Navbar</div>
+    <Box
+    height = {70}
+    color = "midnight"
+    padding={1}
+    shape="roundedBottom"
+    >
+
+    {/* Sign In Link */}
+    <NavLink to="/signin">
+    <Text size="xl" color="white">Sign In</Text>
+    </NavLink>    
+
+
+    </Box>
+
+
+  
 )
 
 export default Navbar
