2aa2ebf2623620afb87bbb1dfd32ba7e6390fdbb
matthew-devonport <roarsintheeast@gmail.com>
2020-04-27 22:54:06 +1200

    created navbar component with correct routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './index.css';
+import 'gestalt/dist/gestalt.css';
 
 import App from './components/App';
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 import Checkout from './components/Checkout'
+import Navbar from './components/Navbar'
 
 import registerServiceWorker from './registerServiceWorker';
 
 
 const Root = () => (
     <Router>
+        <React.Fragment>
+        <Navbar />
         <Switch>
             <Route component={App} exact path ="/" />
             <Route component={SignIn} path ="/signin" />
             <Route component={SignUp} path ="/signup" />
             <Route component={Checkout} path ="/checkout" />
         </Switch>
+        </React.Fragment>
     </Router>
 )
 
