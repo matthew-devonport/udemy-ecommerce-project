e6611265f59b17c420af4184ce6367e96135a48b
matthew-devonport <roarsintheeast@gmail.com>
2020-04-30 12:55:38 +1200

    added search bar for brands

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Box, Heading, Card, Image, Text } from 'gestalt'
+import { Container, Box, Heading, Card, Image, Text, SearchField, Icon } from 'gestalt'
 import { Link } from 'react-router-dom'
 import './App.css';
 import Strapi from 'strapi-sdk-javascript/build/main'
@@ -9,7 +9,8 @@ const strapi = new Strapi(apiUrl)
 
 class App extends Component {
   state = {
-    brands: []
+    brands: [],
+    searchTerm: ''
   }
 
 
@@ -35,51 +36,73 @@ class App extends Component {
     }
   }
 
+  handleChange = ({ value }) => {
+    this.setState({ searchTerm: value })
+  }
+
   render() {
-    const { brands } = this.state
+    const { brands, searchTerm } = this.state
     return (
       <Container>
+        {/* Brands Search Field */}
+        <Box display="flex" justifyContent="center" marginTop={4}>
+          <SearchField
+            id="searchField"
+            accessibilityLabel="Brands Search Field"
+            onChange={this.handleChange}
+            placeholder="Search Brands"
+          />
+          <Box margin={3}>
+            <Icon
+              icon="filter"
+              color={searchTerm ? 'orange' : 'gray'}
+              size={20}
+              accessibilityLabel="filter"
+            />
+          </Box>
+        </Box>
+
         {/* Brands Section */}
         <Box wrap
           display="flex"
           justifyContent="center"
-          marginBottom={2}
-        >
+          marginBottom={2}>
+
           {/* Brands Header */}
           <Heading color="midnight" size="md">
             Brew Brands
         </Heading>
         </Box>
+
         {/* Brands */}
         <Box dangerouslySetInlineStyle={{
           __style: {
             backgroundColor: '#d6c8ec'
           }
         }}
-        shape="rounded"
-        wrap display="flex" justifyContent="around">
+          shape="rounded"
+          wrap display="flex" justifyContent="around">
           {brands.map(brand => (
             <Box paddingY={4} margin={2} width={200} key={brand._id}>
               <Card
-              image={
-                <Box height={200} width={200}>
-                 <Image
-                 fit="cover"
-                 alt="Brand"
-                 naturalHeight={1}
-                 naturalWidth={1}
-                   src={`${apiUrl}${brand.Image[0].url}`}
-                 />
-                </Box>
-              }>
+                image={
+                  <Box height={200} width={200}>
+                    <Image
+                      fit="cover"
+                      alt="Brand"
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brand.Image[0].url}`}
+                    />
+                  </Box>
+                }>
                 <Box display="flex" alignItems="center" justifyContent="center" direction="column">
-                <Text bold size="xl">{brand.name}</Text>
-                <Text>{brand.Description}</Text>
-                <Text bold size="xl"><Link to={`/${brand._id}`}>See Brews</Link></Text>
+                  <Text bold size="xl">{brand.name}</Text>
+                  <Text>{brand.Description}</Text>
+                  <Text bold size="xl"><Link to={`/${brand._id}`}>See Brews</Link></Text>
                 </Box>
               </Card>
             </Box>
-
           ))}
         </Box>
       </Container>
