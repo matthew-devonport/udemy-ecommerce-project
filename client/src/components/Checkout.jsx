3d266743d982147c6411967d22db948b6f9a8837
matthew-devonport <roarsintheeast@gmail.com>
2020-05-03 00:37:53 +1200

    added default text if no items in cart

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Container, Box, Button, Heading, Text, TextField } from 'gestalt'
 import ToastMessage from './ToastMessage'
+import { getCart, calculatePrice } from '../utils'
 
 class Checkout extends React.Component {
     state = {
+        cartItems: [],
         address: '',
         postalCode: '',
         city: '',
@@ -12,15 +14,18 @@ class Checkout extends React.Component {
         toastMessage: '',
     }
 
+    componentDidMount() {
+        this.setState({ cartItems: getCart() })
+    }
 
-    handleChange = ({event, value}) => {
+    handleChange = ({ event, value }) => {
         event.persist()
-        this.setState({ [event.target.name]: value})
+        this.setState({ [event.target.name]: value })
     }
 
     handleConfirmOrder = async event => {
         event.preventDefault()
-     
+
 
         if (this.isFormEmpty(this.state)) {
             this.showToast('Fill in all fields')
@@ -28,80 +33,115 @@ class Checkout extends React.Component {
         }
     }
 
-    isFormEmpty = ({ address, postalCode, city, confirmationEmailAddress}) => {
+    isFormEmpty = ({ address, postalCode, city, confirmationEmailAddress }) => {
         return !address || !postalCode || !city || !confirmationEmailAddress
-       }
-       
-    showToast = toastMessage  => {
-        this.setState({ toast: true, toastMessage})
-        setTimeout(() => this.setState({ toast: false, toastMessage: ''}), 5000)
-  
-      }
+    }
 
-    render () {
-        const {toast, toastMessage} = this.state
+    showToast = toastMessage => {
+        this.setState({ toast: true, toastMessage })
+        setTimeout(() => this.setState({ toast: false, toastMessage: '' }), 5000)
+
+    }
+
+    render() {
+        const { toast, toastMessage, cartItems } = this.state
         return (
             <Container>
-            <Box
-                color="darkWash"
-                margin={4}
-                padding={4}
-                shape="rounded"
-                display="flex"
-                justifyContent="center"
-            >
-
-                {/* Checkout Form */}
-                <form style={{
-                    display: 'inlineBlock',
-                    textAlign: 'center',
-                    maxwidth: 450
-                }}
-                onSubmit={this.handleConfirmOrder}
+                <Box
+                    color="darkWash"
+                    margin={4}
+                    padding={4}
+                    shape="rounded"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    direction="column"
                 >
                     {/* Checkout Form Heading */}
                     <Heading color="midnight">Checkout</Heading>
- 
-                    {/* Shipping Address Input */}
-                    <TextField
-                        id="address"
-                        type="text"
-                        name="address"
-                        placeholder="Shipping Address"
-                        onChange={this.handleChange}
-                    />
-                    {/* Postal Code Input */}
-                    <TextField
-                        id="postalCode"
-                        type="number"
-                        name="postalCode"
-                        placeholder="Postal Code"
-                        onChange={this.handleChange}
-                    />
-                    {/* City Input */}
-                    <TextField
-                        id="city"
-                        type="text"
-                        name="city"
-                        placeholder="City of Residence"
-                        onChange={this.handleChange}
-                    />
-                    {/* Confirmation Email Address Input */}
-                    <TextField
-                        id="confirmationEmailAddress"
-                        type="email"
-                        name="confirmationEmailAddress"
-                        placeholder="Confirmation Email Address"
-                        onChange={this.handleChange}
-                    />                    
-                    <button id="stripe__button" type="submit">Submit</button>
-                </form>
+                    {cartItems.length > 0 ? (<React.Fragment>
+                    {/* User Cart */}
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        direction="column"
+                        marginTop={2}
+                        marginBottom={6}
+                    >
+                        <Text color="darkGray" italic>{cartItems.length} Items for Checkout</Text>
+                        <Box padding={2}>
+                            {cartItems.map(item => (
+                                <Box key={item._id} padding={1}>
+                                 <Text color="midnight">
+                                 {item.name} x {item.quantity} - $
+                                 {item.quantity * item.price}
+                                 </Text>
+                                </Box>
+                            ))}
+                        </Box>
+                        <Text bold>Total Amount: {calculatePrice(cartItems)}</Text>
+                    </Box>
+                    {/* Checkout Form */}
+                    <form style={{
+                        display: 'inlineBlock',
+                        textAlign: 'center',
+                        maxwidth: 450
+                    }}
+                        onSubmit={this.handleConfirmOrder}
+                    >
 
-            </Box>
-            <ToastMessage show={toast} message={toastMessage}/>
-        </Container>   
+                        {/* Shipping Address Input */}
+                        <TextField
+                            id="address"
+                            type="text"
+                            name="address"
+                            placeholder="Shipping Address"
+                            onChange={this.handleChange}
+                        />
+                        {/* Postal Code Input */}
+                        <TextField
+                            id="postalCode"
+                            type="number"
+                            name="postalCode"
+                            placeholder="Postal Code"
+                            onChange={this.handleChange}
+                        />
+                        {/* City Input */}
+                        <TextField
+                            id="city"
+                            type="text"
+                            name="city"
+                            placeholder="City of Residence"
+                            onChange={this.handleChange}
+                        />
+                        {/* Confirmation Email Address Input */}
+                        <TextField
+                            id="confirmationEmailAddress"
+                            type="email"
+                            name="confirmationEmailAddress"
+                            placeholder="Confirmation Email Address"
+                            onChange={this.handleChange}
+                        />
+                        <button id="stripe__button" type="submit">Submit</button>
+                    </form>
+                    </React.Fragment>
+                       ) : (
+                        // Default Text if No Items in Cart
+                       <Box color="darkWash" shape="rounded" padding={4}>
+                       <Heading align="center" color="watermelon" size="xs">
+                        Your Cart is Empty
+                       </Heading>
+                       <Text align="center" italic color="green">
+                       Add some brews!
+                       </Text>
+                       </Box>                                
+                    )}
+                </Box>
+                <ToastMessage show={toast} message={toastMessage} />
+            </Container>
         )
-        
+
     }
 }
 
