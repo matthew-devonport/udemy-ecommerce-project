b53557401e64614234121933794e92f1aab638ac
matthew-devonport <roarsintheeast@gmail.com>
2020-05-02 17:37:20 +1200

    changed navbar for signing out user and correct token backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,57 @@
 import React from 'react';
-import { Box, Text, Heading, Image } from 'gestalt'
+import { Box, Text, Heading, Image, Button } from 'gestalt'
+import { getToken } from '../utils/index'
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
+const Navbar = () => {
+    return getToken() !== null ? <AuthNav /> : <UnAuthNav />
+}
+
+const AuthNav = () => (
+    <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="around"
+        height={100}
+        color="midnight"
+        padding={1}
+        shape="roundedBottom"
+    >
+
+        {/* Checkout link */}
+        <NavLink activeClassName="active" to="/signin">
+            <Text size="xl" color="white">Sign In</Text>
+        </NavLink>
+
+        {/* Title and Logo */}
+        <NavLink activeClassName="active" exact to="/">
+            <Box display="flex" alignItems="center">
+                <Box
+                    margin={2}
+                    height={50}
+                    width={50}>
+                    <Image
+                        alt="Brewhaha logo"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src="./icons/logo.svg"
+                    />
+                </Box>
+                <Heading
+                    size="xs"
+                    color="orange">
+                    BrewHaha
+        </Heading>
+            </Box>
+        </NavLink>
+
+        {/* Signout button */}
+        <Button color="transparent" text="Sign Out" inline size="md"/>
+    </Box>
+)
+
+
+const UnAuthNav = () => (
     <Box
         display="flex"
         alignItems="center"
@@ -19,26 +68,26 @@ const Navbar = () => (
         </NavLink>
 
         {/* Title and Logo */}
-<NavLink activeClassName="active" exact to="/">
-    <Box display="flex" alignItems="center">
-        <Box
-         margin={2}
-         height={50}
-         width={50}>
-            <Image
-             alt="Brewhaha logo"
-             naturalHeight={1}
-             naturalWidth={1}
-             src="./icons/logo.svg" 
-        />
-        </Box>
-            <Heading
-             size="xs"
-             color="orange">
-             BrewHaha
+        <NavLink activeClassName="active" exact to="/">
+            <Box display="flex" alignItems="center">
+                <Box
+                    margin={2}
+                    height={50}
+                    width={50}>
+                    <Image
+                        alt="Brewhaha logo"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src="./icons/logo.svg"
+                    />
+                </Box>
+                <Heading
+                    size="xs"
+                    color="orange">
+                    BrewHaha
             </Heading>
-    </Box>
-</NavLink>
+            </Box>
+        </NavLink>
 
         {/* Sign Up Link */}
         <NavLink activeClassName="active" to="/signup">
