27073f51f69f74ab1695071aa15cb17ceab95184
matthew-devonport <roarsintheeast@gmail.com>
2020-05-02 21:52:11 +1200

    created private route for checkout

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import 'gestalt/dist/gestalt.css';
+import {getToken} from './utils/index'
 
 import App from './components/App';
 import SignIn from './components/SignIn'
@@ -13,6 +14,16 @@ import Brews from './components/Brews'
 import registerServiceWorker from './registerServiceWorker';
 
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        getToken() !== null ?
+        <Component {...props} /> : <Redirect to={{
+            pathname: '/signin',
+            state: { from: props.location}
+        }} />
+    )} />
+)
+
 const Root = () => (
     <Router>
         <React.Fragment>
@@ -21,7 +32,7 @@ const Root = () => (
             <Route component={App} exact path ="/" />
             <Route component={SignIn} path ="/signin" />
             <Route component={SignUp} path ="/signup" />
-            <Route component={Checkout} path ="/checkout" />
+            <PrivateRoute component={Checkout} path ="/checkout" />
             <Route component={Brews} path ="/:brandId" />
         </Switch>
         </React.Fragment>
