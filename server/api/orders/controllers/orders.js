7a0904aa128ac766364067c553f9314a962acee1
matthew-devonport <roarsintheeast@gmail.com>
2020-05-03 22:22:54 +1200

    correctly coded orders.js

--- server/api/orders/controllers/orders.js
+++ server/api/orders/controllers/orders.js
@@ -1,79 +1,33 @@
-'use strict';
+// Goes at top of file
+const stripe = require('stripe')(sk_test_BOdVKRcpHlO47qzBZXRDK5D200ei8jZsqL);
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+create: async ctx => {
+  const {
+    address,
+    amount,
+    brews,
+    postalCode,
+    token,
+    city
+  } = ctx.request.body;
 
-const { sanitizeEntity } = require('strapi-utils');
+  // Send charge to Stripe
+  const charge = await stripe.charges.create({
+    amount: amount * 100,
+    currency: "usd",
+    description: `Order ${new Date(Date.now())} - User ${ctx.state.user._id}`,
+    source: token
+  });
 
-module.exports = {
-  /**
-   * Retrieve records.
-   *
-   * @return {Array}
-   */
+  // Create order in database
+  const order = await strapi.services.orders.add({
+    user: ctx.state.user._id,
+    address,
+    amount,
+    brews,
+    postalCode,
+    city
+  });
 
-  async find(ctx) {
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services.restaurant.search(ctx.query);
-    } else {
-      entities = await strapi.services.restaurant.find(ctx.query);
-    }
-
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
-  },
-
-  async findOne(ctx) {
-    const { id } = ctx.params;
-
-    const entity = await strapi.services.restaurant.findOne({ id });
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  },
-
-  count(ctx) {
-    if (ctx.query._q) {
-      return strapi.services.restaurant.countSearch(ctx.query);
-    }
-    return strapi.services.restaurant.count(ctx.query);
-  },
-
-  async create(ctx) {
-    let entity;
-    if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.restaurant.create(data, { files });
-    } else {
-      entity = await strapi.services.restaurant.create(ctx.request.body);
-    }
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  },
-  
-  async update(ctx) {
-    const { id } = ctx.params;
-
-    let entity;
-    if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.restaurant.update({ id }, data, {
-        files,
-      });
-    } else {
-      entity = await strapi.services.restaurant.update({ id }, ctx.request.body);
-    }
-
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  },
-  
-  async delete(ctx) {
-    const { id } = ctx.params;
-
-    const entity = await strapi.services.restaurant.delete({ id });
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  }, 
-  
-
-
-};
-
+  return order;
+}
